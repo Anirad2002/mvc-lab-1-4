8b9663e0a3ab451af4ebd670d1eb2f1b5be980fa
Pasiura Daryna <107425596+Anirad2002@users.noreply.github.com>
2024-04-09 09:48:26 +0200

    Add files via upload

---
+++ zad2/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const studentsController = require('./controllers/students');
+const errorController = require('./controllers/error');
+
+app.get('/', studentsController.getAddNewStudentPage);
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('Home', { title: 'Home' });
+});
+
+app.get('/success', (req, res) => {
+  res.render('Success', { title: 'Success' });
+});
+
+app.get('/students-list', (req, res) => {
+  res.render('List', { title: 'List' });
+});
+
+app.post('/add-student', (req, res) => {
+  
+  res.redirect('/students-list');
+});
+
+app.use(errorController.getNotFoundPage);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
+
---
+++ zad3/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const studentsController = require('./controllers/students');
+const errorController = require('./controllers/error');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/add-student', studentsController.addStudent);
+app.get('/success', studentsController.getAddingNewStudentSuccessPage);
+
+
+app.get('/', studentsController.getAddNewStudentPage);
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.render('Home', { title: 'Home' });
+});
+
+app.get('/success', (req, res) => {
+  res.render('Success', { title: 'Success' });
+});
+
+app.get('/students-list', (req, res) => {
+  res.render('List', { title: 'List' });
+});
+
+app.post('/add-student', (req, res) => {
+  
+  res.redirect('/students-list');
+});
+
+app.use(errorController.getNotFoundPage);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
+
